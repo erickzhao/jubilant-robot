98a2f176a75e5f56b692f747ffc80b8c686dcb3d
Erick Zhao <erick@hotmail.ca>
2021-03-16 16:57:17 -0700

    feat: add basic APIs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Browser, remote } from 'webdriverio';
 export class FiddleBot {
   private constructor(
+    /**
+     * The underlying WebDriver.IO browser driving Electron Fiddle
+     */
     public readonly browser: Browser<"async">
   ) {
   }
+  /**
+   * Asynchronous static constructor for FiddleBot
+   * @returns An instance of FiddleBot
+   */
   public static CreateAsync = async () => {
     const browser = await remote({
       capabilities: {
@@ -13,17 +20,66 @@ export class FiddleBot {
         },
       },
     })
+
+    // remove initial popup box
+    const apiLink = await browser.$(`span=I'll figure it out`);
+    await apiLink.click();
+
     return new FiddleBot(browser);
   }
 
+  /**
+   * Loads a gist into Electron Fiddle
+   * @param gistUrl The URL of the gist to load
+   */
+  public async loadGist(gistUrl: string) {
+    const input = await this.browser.$(`[placeholder='https://gist.github.com/...']`);
+    await input.setValue(gistUrl);
+
+    await this.browser.pause(1000);
+
+    const button = await this.browser.$('span=Load Fiddle');
+    await button.click();
+  }
+
+  /**
+   * Sets the first version 
+   * @param version The filt
+   */
+  public async selectVersion(version: string) {
+    const button = await this.browser.$(`button.version-chooser`);
+    await button.click();
+    await this.browser.pause(1000);
+    const input = await this.browser.$(`[placeholder='Filter...']`);
+    await input.setValue(version);
+    await this.browser.keys(['Enter'])
+  }
+
+  /**
+   * Executes the current Fiddle
+   */
+  public async runFiddle() {
+    const button = await this.browser.$('.button-run');
+    await button.waitForClickable();
+    await button.click();
+  }
+
+  /**
+   * Closes Electron Fiddle
+   */
   public async close() {
-    return this.browser.deleteSession()
+    this.browser.deleteSession()
   }
 }
 
+// Remove this tester code
 (async () => {
   const fb = await FiddleBot.CreateAsync();
-  const apiLink = await fb.browser.$(`span=I'll figure it out`);
-  await apiLink.click();
+  await fb.loadGist(`https://gist.github.com/ckerr/af3e1a018f5dcce4a2ff40004ef5bab5`);
+  await fb.selectVersion('11.0.0');
+  await fb.runFiddle();
+
+  browser.pause(10000);
+
   await fb.close();
 })()
