aed328a3b807d0469bd7601b4944c0f03bdf613f
Erick Zhao <erick@hotmail.ca>
2021-03-16 15:44:52 -0700

    feat: create FiddleBot class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
-import { remote } from 'webdriverio';
+import { Browser, remote } from 'webdriverio';
+export class FiddleBot {
+  private constructor(
+    public readonly browser: Browser<"async">
+  ) {
+  }
+  public static CreateAsync = async () => {
+    const browser = await remote({
+      capabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          binary: `/Applications/Electron Fiddle.app/Contents/MacOS/electron-fiddle`,
+        },
+      },
+    })
+    return new FiddleBot(browser);
+  }
+
+  public async close() {
+    return this.browser.deleteSession()
+  }
+}
 
 (async () => {
-  const browser = await remote({
-    capabilities: {
-      browserName: 'chrome',
-      'goog:chromeOptions': {
-        binary: `/Applications/Electron Fiddle.app/Contents/MacOS/electron-fiddle`,
-      },
-    },
-  })
-  const apiLink = await browser.$(`span=I'll figure it out`);
-  apiLink.click();
-  await browser.debug();
-  await browser.deleteSession()
+  const fb = await FiddleBot.CreateAsync();
+  const apiLink = await fb.browser.$(`span=I'll figure it out`);
+  await apiLink.click();
+  await fb.close();
 })()
